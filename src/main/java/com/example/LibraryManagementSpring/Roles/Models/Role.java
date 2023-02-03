135acfd1ae22061ac53114e3f11982e7f783cc35
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-04 01:04:41 +0300

    update: add java bean setters and getters

--- src/main/java/com/example/LibraryManagementSpring/Roles/Models/Role.java
+++ src/main/java/com/example/LibraryManagementSpring/Roles/Models/Role.java
@@ -1,8 +1,13 @@
 package com.example.LibraryManagementSpring.Roles.Models;
 
 import com.example.LibraryManagementSpring.Users.Models.CustomUser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
+
 @Entity
 public class Role {
     @Id
@@ -10,12 +15,9 @@ public class Role {
     private Integer roleId;
     private String roleName;
 
-    @Override
-    public String toString() {
-        return "Role{" +
-                "roleId=" + roleId +
-                ", roleName='" + roleName + '\'' +
-                '}';
+    public Role(Integer roleId,String roleName) {
+        this.roleName = roleName;
+        this.roleId = roleId;
     }
 
     public Integer getRoleId() {
@@ -26,11 +28,23 @@ public class Role {
         this.roleId = roleId;
     }
 
+    public Role() {
+    }
+
     public String getRoleName() {
         return roleName;
     }
 
+
     public void setRoleName(String roleName) {
         this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId=" + roleId +
+                ", roleName='" + roleName + '\'' +
+                '}';
     }
 }
