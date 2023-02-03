975f2722ebb01543df7be45f3443305e85a47c71
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-04 01:04:03 +0300

    update: make repository visible

--- src/main/java/com/example/LibraryManagementSpring/LibraryManagementSpringApplication.java
+++ src/main/java/com/example/LibraryManagementSpring/LibraryManagementSpringApplication.java
@@ -1,12 +1,13 @@
 package com.example.LibraryManagementSpring;
 
+import com.example.LibraryManagementSpring.Roles.Repositories.RoleRepository;
 import com.example.LibraryManagementSpring.Users.Repositories.CustomUserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = CustomUserRepository.class)
+@EnableJpaRepositories(basePackageClasses = {CustomUserRepository.class, RoleRepository.class})
 public class LibraryManagementSpringApplication {
 
 	public static void main(String[] args) {
