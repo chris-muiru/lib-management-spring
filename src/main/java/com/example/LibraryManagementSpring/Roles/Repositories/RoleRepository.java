6c52be3cc217febd6f132b3b96e6acafa0eaf023
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-05 00:28:26 +0300

    update: add query method

--- src/main/java/com/example/LibraryManagementSpring/Roles/Repositories/RoleRepository.java
+++ src/main/java/com/example/LibraryManagementSpring/Roles/Repositories/RoleRepository.java
@@ -1,9 +1,10 @@
 package com.example.LibraryManagementSpring.Roles.Repositories;
 
-import com.example.LibraryManagementSpring.Roles.Models.Role;
+import com.example.LibraryManagementSpring.Roles.Entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Integer> {
+    boolean existsByRoleName(String roleName);
 }
