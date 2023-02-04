b225491adfa3a114acf6f35b695c9e19321fd7f1
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-05 00:24:10 +0300

    update: use users

--- src/main/java/com/example/LibraryManagementSpring/Users/Controllers/CustomUserController.java
+++ src/main/java/com/example/LibraryManagementSpring/Users/Controllers/CustomUserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="/")
+@RequestMapping(path="/users")
 public class CustomUserController {
 //    @Autowired
     @GetMapping
