5b513a97764a5a7e877f2e58d2f2a46276efc298
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-05 00:31:18 +0300

    update: change from Models to entities

--- src/main/java/com/example/LibraryManagementSpring/Auth/Services/SignUpService.java
+++ src/main/java/com/example/LibraryManagementSpring/Auth/Services/SignUpService.java
@@ -1,7 +1,6 @@
 package com.example.LibraryManagementSpring.Auth.Services;
 
-import com.example.LibraryManagementSpring.Auth.Dto.CustomUserDto;
-import com.example.LibraryManagementSpring.Users.Models.CustomUser;
+import com.example.LibraryManagementSpring.Users.Entities.CustomUser;
 import com.example.LibraryManagementSpring.Users.Repositories.CustomUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
