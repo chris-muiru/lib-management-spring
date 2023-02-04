278defbf4125aae733cc8c6cea7c3d134a9f7246
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-05 00:24:30 +0300

    clean: rm commented code

--- src/main/java/com/example/LibraryManagementSpring/Config/CustomUserDetails.java
+++ src/main/java/com/example/LibraryManagementSpring/Config/CustomUserDetails.java
@@ -1,16 +1,13 @@
 package com.example.LibraryManagementSpring.Config;
 
-import com.example.LibraryManagementSpring.Users.Models.CustomUser;
-import org.apache.catalina.realm.GenericPrincipal;
+import com.example.LibraryManagementSpring.Users.Entities.CustomUser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CustomUserDetails implements UserDetails {
     private String username;
@@ -25,15 +22,6 @@ public class CustomUserDetails implements UserDetails {
         this.password = user.getPassword();
         this.active= user.getActive();
         this.authorities = Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getRoleName()));
-
-//        this.authorities= Arrays.stream(
-//                        user.getRoles().split(","))
-//                .map(SimpleGrantedAuthority::new)
-//                .collect(Collectors.toList());
-//        this.authorities= user.getRole()
-//                .stream()
-//                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
-//                .collect(Collectors.toList());
     }
 
     @Override
