57bd5eabd723218a33628f3addef3828c43abee5
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-05 00:26:05 +0300

    update: if role exist change user role to role

--- src/main/java/com/example/LibraryManagementSpring/Users/Services/CustomUserService.java
+++ src/main/java/com/example/LibraryManagementSpring/Users/Services/CustomUserService.java
@@ -1,29 +1,39 @@
 package com.example.LibraryManagementSpring.Users.Services;
 
-import com.example.LibraryManagementSpring.Roles.Models.Role;
-import com.example.LibraryManagementSpring.Users.Models.CustomUser;
+import com.example.LibraryManagementSpring.Roles.Entities.Role;
+import com.example.LibraryManagementSpring.Roles.Repositories.RoleRepository;
+import com.example.LibraryManagementSpring.Users.Entities.CustomUser;
 import com.example.LibraryManagementSpring.Users.Repositories.CustomUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomUserService {
 
     @Autowired
     CustomUserRepository customUserRepository;
+    @Autowired
+    RoleRepository roleRepository;
     public List<CustomUser> fetchAllCustomUsers(){
         return customUserRepository.findAll();
     }
 
-    public CustomUser setUserRole(String username,Role role) throws UsernameNotFoundException {
-        CustomUser user = customUserRepository.findByUsername(username).orElseThrow(
+    public CustomUser setUserRole(CustomUser user) throws Exception {
+        CustomUser customUser = customUserRepository.findByUsername(user.getUsername()).orElseThrow(
                 ()-> new UsernameNotFoundException("user doesnt exist")
         );
-        user.setRole(role);
-        return user;
+        if(roleRepository.existsByRoleName(user.getRole().getRoleName())){
+            System.out.println(user.toString());
+            customUser.setRole(user.getRole());
+            return customUserRepository.save(customUser);
+        }
+        throw new Exception("role doesn't exist");
+
     }
 
     public CustomUser deactivateUser(String username) {
