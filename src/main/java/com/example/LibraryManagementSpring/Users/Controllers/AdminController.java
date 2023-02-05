6bffba910b9fa2e427290c6c68823cd525636357
kris-slinger <krisdjangodeveloper@gmail.com>
2023-02-05 14:34:47 +0300

    update: map request models to customUser

--- src/main/java/com/example/LibraryManagementSpring/Users/Controllers/AdminController.java
+++ src/main/java/com/example/LibraryManagementSpring/Users/Controllers/AdminController.java
@@ -3,6 +3,7 @@ package com.example.LibraryManagementSpring.Users.Controllers;
 import com.example.LibraryManagementSpring.Roles.Entities.Role;
 import com.example.LibraryManagementSpring.Users.Entities.CustomUser;
 import com.example.LibraryManagementSpring.Users.Models.Requests.SetUserRole;
+import com.example.LibraryManagementSpring.Users.Models.Requests.UserDetailsRequest;
 import com.example.LibraryManagementSpring.Users.Services.CustomUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +13,32 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path="/admin")
-public class AdminController{
-    @Autowired
+    public class AdminController{
+        @Autowired
     private CustomUserService customUserService;
 
-    @GetMapping("users")
+    @GetMapping("/users")
     public List<CustomUser> getAllCustomUsers(){
         return customUserService.fetchAllCustomUsers();
     }
 
     @PostMapping("/users/set-role")
-    public CustomUser assignUserRole(@RequestBody SetUserRole userDetails) throws Exception {
+    public CustomUser assignUserRole(@RequestBody SetUserRole userDetails){
         ModelMapper modelMapper = new ModelMapper();
         CustomUser customUser = modelMapper.map(userDetails,CustomUser.class);
         System.out.println(customUser.toString());
         return customUserService.setUserRole(customUser);
     }
    @PostMapping("/users/deactivate")
-    public CustomUser deactivateUser(@RequestBody String username){
-       return customUserService.deactivateUser(username);
+    public CustomUser deactivateUser(@RequestBody String userDetails){
+        ModelMapper modelMapper = new ModelMapper();
+        CustomUser userInstance  = modelMapper.map(userDetails,CustomUser.class);
+       return customUserService.deactivateUser(userInstance);
     }
     @DeleteMapping("/users/delete")
-    public void deleteUserByEmail(@RequestBody String email){
-         customUserService.deleteUserByEmail(email);
+    public void deleteUserByEmail(@RequestBody UserDetailsRequest userDetails){
+        ModelMapper modelMapper = new ModelMapper();
+        CustomUser userInstance  = modelMapper.map(userDetails,CustomUser.class);
+         customUserService.deleteUserByEmail(userInstance);
     }
 }
